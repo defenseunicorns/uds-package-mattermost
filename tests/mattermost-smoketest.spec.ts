914c85c7093337d794c9051f7df01d3653a1733b
Marshall Cottrell <marshall@defenseunicorns.com>
2024-05-14 13:32:39 -0400

    test sending a message

--- tests/mattermost-smoketest.spec.ts
+++ tests/mattermost-smoketest.spec.ts
@@ -9,32 +9,33 @@ export const currentDateTime = () => {
 };
 
 async function loginToMattermost(page: Page) {
-  await page.goto("https://chat.uds.dev/");
-  await page.goto("https://chat.uds.dev/landing#/");
+  await page.goto("https://chat.uds.dev/login");
   await page.getByRole("link", { name: "View in Browser" }).click();
   await page.getByRole("link", { name: "Gitlab Icon GitLab" }).click();
-  await page.getByLabel("Username or email").click();
   await page.getByLabel("Username or email").fill("doug");
-  await page.getByLabel("Password").click();
   await page.getByLabel("Password").fill("unicorn123!@#");
   await page.getByRole("button", { name: "Log In" }).click();
-  await page.waitForURL(
-    /\/preparing-workspace|\/unicorns\/channels\/town-square/,
-  );
 }
 
 async function setupWorkspace(page: Page) {
-  await page.getByPlaceholder("Organization name").click();
   await page.getByPlaceholder("Organization name").fill("Unicorns");
   await page.getByTestId("continue").click();
   await page.getByRole("button", { name: "Skip" }).click();
   await page.getByRole("button", { name: "Finish setup" }).click();
   await page.waitForURL(/\/unicorns\/channels\/town-square/);
   await page.locator("button").filter({ hasText: "7" }).click();
+  
   const button = page.locator('role=button[name="Not now"]');
   if (await button.isVisible()) {
     await button.click();
   }
+}
+
+async function sendMessage(page: Page) {
+  const messageBox = page.getByRole('textbox', { name: 'Write to Town Square' });
+  await messageBox.fill('Hello world!');
+  await messageBox.press('Enter');
+  await expect(page.locator('#post-list .post-message__text').last()).toContainText('Hello world!');
 }
 
 test("test mattermost login, init and message", async ({ page, context }) => {
@@ -48,10 +49,9 @@ test("test mattermost login, init and message", async ({ page, context }) => {
     await page.waitForURL(/\/unicorns\/channels\/town-square/);
   }
 
-  expect(
-    page.getByLabel("set status").locator("div").filter({ hasText: "@doug" }),
-  ).toBeTruthy();
   expect(page.url()).toContain("/unicorns/channels/town-square");
+
+  await sendMessage(page);
 
   const cookies = await context.cookies();
   const keycloakCookie = cookies.find(
@@ -61,5 +61,4 @@ test("test mattermost login, init and message", async ({ page, context }) => {
   expect(keycloakCookie).toBeDefined();
   expect(keycloakCookie?.value).not.toBe("");
   expect(keycloakCookie?.domain).toContain("sso.uds.dev");
-
 });
