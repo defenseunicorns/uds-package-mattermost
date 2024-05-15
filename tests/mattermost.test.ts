59bc494c959695f460c12bcd866b9e1f40239e5b
Marshall Cottrell <marshall@defenseunicorns.com>
2024-05-15 14:23:05 -0400

    simplify test names

--- tests/mattermost.test.ts
+++ tests/mattermost.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("test mattermost login, init and message", async ({ page }) => {
+test("send a message message", async ({ page }) => {
   await page.goto("/unicorns/channels/town-square");
 
   const messageBox = page.getByRole('textbox', { name: 'Write to Town Square' });
