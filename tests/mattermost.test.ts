4368f5947a0b398eca64d1b4672783b5dabd7fad
Marshall Cottrell <marshall@defenseunicorns.com>
2024-05-15 15:59:35 -0400

    test file uploads

--- tests/mattermost.test.ts
+++ tests/mattermost.test.ts
@@ -1,13 +1,57 @@
 import { test, expect } from "@playwright/test";
+import path from 'path';
+import consumers from 'stream/consumers';
 
-test("send a message message", async ({ page }) => {
+function randomMessage(extra: string = "") {
+  return `hello universe ${Math.floor((Math.random() * 1000))}-C3!` + extra;
+}
+
+test("send a message", async ({ page }) => {
+  await page.goto("/unicorns/channels/town-square");
+
+  const message = randomMessage();
+  const messageBox = page.getByRole('textbox', { name: 'Write to Town Square' });
+
+  await messageBox.fill(message);
+  await page.getByRole('button', { name: 'Send a message' }).click();
+
+  await expect(messageBox).toBeEmpty();
+  expect(page.locator('#post-list .post', { hasText: message })).toBeDefined();
+});
+
+test("send a message with attachment", async ({ page }) => {
   await page.goto("/unicorns/channels/town-square");
 
   const messageBox = page.getByRole('textbox', { name: 'Write to Town Square' });
 
-  await messageBox.fill('Hello world!');
-  await page.getByRole('button', { name: 'Send a message' }).click();
+  const message = randomMessage('\ncheckout this attachment...');
+  await messageBox.fill(message);
 
+  const upload = Buffer.from('# My Cool Project');
+
+  await page.getByLabel('Upload files')
+    .setInputFiles({
+      name: 'README.md',
+      buffer: upload,
+      mimeType: 'text/plain'
+    });
+
+  await page.getByRole('button', { name: 'Send a message' }).click();
   await expect(messageBox).toBeEmpty();
-  await expect(page.locator('#post-list .post-message__text').last()).toContainText('Hello world!');
+
+  const downloadEvent = page.waitForEvent('download');
+
+  const post = page.locator('#post-list .post__body', { hasText: message });
+
+  await expect(post).toBeDefined();
+
+  await post.getByRole('link', { name: 'download' }).click();
+
+  const download = await downloadEvent;
+
+  expect(download.suggestedFilename()).toBe('README.md');
+
+  const rs = await download.createReadStream();
+  const data = await consumers.buffer(rs);
+  expect(data.equals(upload)).toBe(true);
 });
--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,11 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
 export const playwrightDir = '.playwright';
 export const authFile = `${playwrightDir}/auth/user.json`;
 
@@ -13,16 +7,12 @@ export const authFile = `${playwrightDir}/auth/user.json`;
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  forbidOnly: !!process.env.CI, // fail CI if you accidently leave `test.only` in source
+  retries: process.env.CI ? 1 : 0,
+  workers: 1,
   reporter: [
+    // Reporter to use. See https://playwright.dev/docs/test-reporters
     ['html', { outputFolder: `${playwrightDir}/reports`, open: 'never' }],
     ['json', { outputFile: `${playwrightDir}/reports/test-results.json`, open: 'never' }],
     ['list']
@@ -30,18 +20,13 @@ export default defineConfig({
 
   outputDir: `${playwrightDir}/output`,
 
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL || 'https://chat.uds.dev',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    baseURL: process.env.BASE_URL || 'https://chat.uds.dev', // for `await page.goto('/')` etc
+    trace: 'on-first-retry', // collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer
   },
 
-  /* Configure projects for major browsers */
   projects: [
-    { name: 'setup', testMatch: /.*\.setup\.ts/ },
+    { name: 'setup', testMatch: /.*\.setup\.ts/ }, // authentication
 
     ...[
       'Desktop Chrome',
