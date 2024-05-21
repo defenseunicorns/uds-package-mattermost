074749c5aa7d54712bb2da11a5ee78a0987a732b
zamaz <71521611+zachariahmiller@users.noreply.github.com>
2024-05-21 14:15:35 -0400

    chore: add basic smoke test for mattermost with sso (#65)
    Fixes #75

---
+++ tests/auth.setup.ts
@@ -0,0 +1,40 @@
+import { test as setup, expect } from '@playwright/test';
+import { authFile } from './playwright.config';
+
+setup('authenticate', async ({ page, context, baseURL }) => {
+  await page.goto('/login');
+
+  await page.getByRole("link", { name: "View in Browser" }).click();
+  await page.getByRole("link", { name: "Gitlab Icon GitLab" }).click();
+  await page.getByLabel("Username or email").fill("doug");
+  await page.getByLabel("Password").fill("unicorn123!@#");
+  await page.getByRole("button", { name: "Log In" }).click();
+
+  // ensure auth cookies were set
+  const cookies = await context.cookies();
+  const keycloakCookie = cookies.find(
+    (cookie) => cookie.name === "KEYCLOAK_SESSION",
+  );
+
+  expect(keycloakCookie).toBeDefined();
+  expect(keycloakCookie?.value).not.toBe("");
+  expect(keycloakCookie?.domain).toContain("sso.");
+
+  await page.context().storageState({ path: authFile });
+
+  await page.waitForURL(url =>
+    url.pathname === '/' ||
+    url.pathname === '/preparing-workspace' ||
+    url.pathname === '/unicorns/channels/town-square'
+  , { waitUntil: 'networkidle' });
+
+  // one-time workspace setup (when login redirects to "/preparing-workspace")
+  if (page.url().endsWith('/preparing-workspace')) {
+    const orgInput = page.getByPlaceholder("Organization name");
+    await orgInput.isVisible();
+    await orgInput.fill("Unicorns");
+    await page.getByTestId("continue").click();
+    await page.getByRole("button", { name: "Skip" }).click();
+    await page.getByRole("button", { name: "Finish setup" }).click();
+  };
+})
---
+++ tests/mattermost.test.ts
@@ -0,0 +1,104 @@
+import { test, expect, request, APIRequestContext, Page } from "@playwright/test";
+import consumers from 'stream/consumers';
+
+type Channel = { id: string; name: string; display_name: string; };
+
+let apiCtx: APIRequestContext;
+let channel: Channel;
+
+test.beforeEach(async ({ context, baseURL }) => {
+  const cookies = await context.cookies();
+  const token = cookies.find(c => c.name === 'MMAUTHTOKEN');
+
+  expect(token?.value).toBeDefined();
+
+  apiCtx = await request.newContext({
+    baseURL,
+    extraHTTPHeaders: {
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${token?.value}`,
+      'X-Requested-With': 'XMLHttpRequest', // without this you get "Invalid or expired session, please login again."
+    }
+  });
+
+  // poll channel list in case default channels haven't been created yet
+  await expect(async () => {
+    const channels = await apiCtx.get('/api/v4/channels')
+      .then(res => res.json());
+
+    expect(channels.length).toBeGreaterThanOrEqual(1);
+
+    channel = channels.find((c: Channel) => c.display_name === 'Town Square');
+    expect(channel).toBeDefined();
+    expect(channel.display_name).toBe('Town Square');
+  }).toPass({
+    timeout: 60_000,
+  });
+});
+
+function randomMessage(extra: string = "") {
+  return `hello universe ${Math.floor((Math.random() * 1000))}-C3!` + extra;
+}
+
+async function createPost(page: Page, data: { channel_id: string, message: string; file_ids?: string[] }) {
+  const req = await apiCtx.post('/api/v4/posts', {
+    data,
+  });
+
+  expect(req).toBeOK();
+
+  const post = await req.json();
+
+  expect(post.id).toBeDefined();
+  expect(post.message).toBe(data.message);
+
+  return post;
+}
+
+test("send a message", async ({ page }) => {
+  const post = await createPost(page, {
+    channel_id: channel.id,
+    message: randomMessage(),
+  });
+
+  await page.goto('/unicorns/channels/town-square');
+
+  const el = page.locator(`#post_${post.id}`);
+  await expect(el).toContainText(post.message);
+});
+
+test("send a message with attachment", async ({ page }) => {
+  const file = {
+    name: 'README.md',
+    mimeType: 'text/plain',
+    buffer: Buffer.from('# My Cool Project'),
+  };
+
+  const upload = await apiCtx.fetch('/api/v4/files', {
+    method: 'POST',
+    multipart: {
+      channel_id: channel.id,
+      files: file,
+    },
+  });
+
+  expect(upload).toBeOK();
+
+  const res = await upload.json();
+
+  expect(res.file_infos.length).toBe(1);
+  expect(res.file_infos[0].name).toBe(file.name);
+
+  const message = randomMessage('\ncheckout this attachment...');
+  const post = await createPost(page, {
+    channel_id: channel.id,
+    message,
+    file_ids: [ res.file_infos[0].id ],
+  });
+
+  await page.goto('/unicorns/channels/town-square');
+
+  const el = page.locator(`#post_${post.id}`);
+  await expect(el).toContainText(post.message);
+
+});
