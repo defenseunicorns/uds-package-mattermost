05cbd157de4a0fea0111fd227b907c3f51979fb5
zamaz <71521611+zachariahmiller@users.noreply.github.com>
2024-10-07 18:10:56 -0400

    shellcheck

--- plugins/entrypoint.sh
+++ plugins/entrypoint.sh
@@ -10,7 +10,7 @@ for plugin_tar in ${PLUGINS_TAR};
 do
   plugin_tar="/extra-plugins/${plugin_tar##*/}"
   echo "extracting $plugin_tar ..."
-  tar -xf $plugin_tar
+  tar -xf "$plugin_tar"
 done
 
 echo "finished loading plugins"
